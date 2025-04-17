319250301faa59dffdb7a1ae43b6c32d2ed3362c
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-04-17 16:48:17 +0200

    create app theme class

---
+++ lib/core/config/app_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: Color(0xffE8F5E9),
+    fontFamily: "Cairo",
+  );
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: Color(0xff0D1B2A),
+    fontFamily: "Cairo",
+  );
+}
