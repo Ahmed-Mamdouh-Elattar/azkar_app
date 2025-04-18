bd8e65c6fd5377fed0d4da47473ce44da54a95b7
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-04-18 21:59:39 +0200

    edit splash screen

---
+++ lib/core/assets/assets.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for mediaAnimationsWelcome
+  /// assets/media/animations/welcome.json
+  static const String mediaAnimationsWelcome = "assets/media/animations/welcome.json";
+
+  /// Assets for mediaImagesSplashLogo
+  /// assets/media/images/splash_logo.png
+  static const String mediaImagesSplashLogo = "assets/media/images/splash_logo.png";
+
+  /// Assets for mediaImagesSplashLogo2
+  /// assets/media/images/splash_logo_2.png
+  static const String mediaImagesSplashLogo2 = "assets/media/images/splash_logo_2.png";
+}
+
