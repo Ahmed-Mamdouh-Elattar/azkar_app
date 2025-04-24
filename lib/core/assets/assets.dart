f9ac0d265c552cbc26a8112292787d853c1a98da
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-04-24 21:56:43 +0200

    generate assets

--- lib/core/assets/assets.dart
+++ lib/core/assets/assets.dart
@@ -18,6 +18,66 @@ class Assets {
   /// assets/media/animations/welcome.json
   static const String mediaAnimationsWelcome = "assets/media/animations/welcome.json";
 
+  /// Assets for mediaIconsAblution
+  /// assets/media/icons/ablution.svg
+  static const String mediaIconsAblution = "assets/media/icons/ablution.svg";
+
+  /// Assets for mediaIconsAfterPraying
+  /// assets/media/icons/after_praying.svg
+  static const String mediaIconsAfterPraying = "assets/media/icons/after_praying.svg";
+
+  /// Assets for mediaIconsAfterSleeping
+  /// assets/media/icons/after_sleeping.svg
+  static const String mediaIconsAfterSleeping = "assets/media/icons/after_sleeping.svg";
+
+  /// Assets for mediaIconsAzan
+  /// assets/media/icons/azan.svg
+  static const String mediaIconsAzan = "assets/media/icons/azan.svg";
+
+  /// Assets for mediaIconsBath
+  /// assets/media/icons/bath.svg
+  static const String mediaIconsBath = "assets/media/icons/bath.svg";
+
+  /// Assets for mediaIconsBeforeSleeping
+  /// assets/media/icons/before_sleeping.svg
+  static const String mediaIconsBeforeSleeping = "assets/media/icons/before_sleeping.svg";
+
+  /// Assets for mediaIconsFoodAndDrink
+  /// assets/media/icons/food-and-drink.svg
+  static const String mediaIconsFoodAndDrink = "assets/media/icons/food-and-drink.svg";
+
+  /// Assets for mediaIconsHome
+  /// assets/media/icons/home.svg
+  static const String mediaIconsHome = "assets/media/icons/home.svg";
+
+  /// Assets for mediaIconsMoon
+  /// assets/media/icons/moon.svg
+  static const String mediaIconsMoon = "assets/media/icons/moon.svg";
+
+  /// Assets for mediaIconsMoonNight
+  /// assets/media/icons/moon-night.svg
+  static const String mediaIconsMoonNight = "assets/media/icons/moon-night.svg";
+
+  /// Assets for mediaIconsMosque
+  /// assets/media/icons/mosque.svg
+  static const String mediaIconsMosque = "assets/media/icons/mosque.svg";
+
+  /// Assets for mediaIconsPraying
+  /// assets/media/icons/praying.svg
+  static const String mediaIconsPraying = "assets/media/icons/praying.svg";
+
+  /// Assets for mediaIconsSun
+  /// assets/media/icons/sun.svg
+  static const String mediaIconsSun = "assets/media/icons/sun.svg";
+
+  /// Assets for mediaIconsTasbeeh
+  /// assets/media/icons/tasbeeh.svg
+  static const String mediaIconsTasbeeh = "assets/media/icons/tasbeeh.svg";
+
+  /// Assets for mediaIconsTraveling
+  /// assets/media/icons/traveling.svg
+  static const String mediaIconsTraveling = "assets/media/icons/traveling.svg";
+
   /// Assets for mediaImagesSplashLogo
   /// assets/media/images/splash_logo.png
   static const String mediaImagesSplashLogo = "assets/media/images/splash_logo.png";
