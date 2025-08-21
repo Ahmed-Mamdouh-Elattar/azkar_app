a1e5e3fce8ba3c5ac677ec08cb28f6f55b537be8
Ahmed-Mamdouh-Elattar <Ahmed.Mamdouh.Elattar@gmail.com>
2025-08-21 08:46:28 +0300

    create funtion to pick random ziker

---
+++ lib/core/helper/pick_random_ziker.dart
@@ -0,0 +1,14 @@
+import 'dart:math';
+
+final List<String> azkar = [
+  "سبحان الله",
+  "الحمد لله",
+  "الله أكبر",
+  "لا إله إلا الله",
+  "لا حول ولا قوة إلا بالله",
+];
+
+String getRandomZikr() {
+  final random = Random();
+  return azkar[random.nextInt(azkar.length)];
+}
